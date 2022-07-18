6112addd5cb843e10c2112acc7f8580df70160f5
Kotlin5662 <80145877+Cmr-Ms@users.noreply.github.com>
2022-07-18 15:43:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-navigator.geolocation.getCurrentPosition(
-    function(a){
-        r = `${a.coords.latitude}, ${a.coords.longitude}`
-        navigator.clipboard.writeText(r)
-        if(new URL(location.href).searchParams.get("redirect")){location.href=new URL(location.href).searchParams.get("redirect")}
-    },
-    function(a){}
-)
+javascript:
+    navigator.geolocation.getCurrentPosition(
+        function(a){
+            r = `${a.coords.latitude}, ${a.coords.longitude}`
+            navigator.clipboard.writeText(r)
+            if(new URL(location.href).searchParams.get("redirect")){location.href=new URL(location.href).searchParams.get("redirect")}
+        },
+        function(a){}
+    )
