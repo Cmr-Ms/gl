5e810d6af866e1974197a079315c6d0e5f32c792
Kotlin5662 <80145877+Cmr-Ms@users.noreply.github.com>
2022-07-18 15:32:19 +0900

    Update and rename index.html to main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+navigator.geolocation.getCurrentPosition(
+    function(a){
+        r = `${a.coords.latitude}, ${a.coords.longitude}`
+        navigator.clipboard.writeText(r)
+        if(new URL(location.href).searchParams.get("redirect")){location.href=new URL(location.href).searchParams.get("redirect")}
+    },
+    function(a){}
+)
